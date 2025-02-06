b9ca103d29763c7f5c69d5b4a258de829099426e
Ramazan Öztürk <rozturk3451@gmail.com>
2025-02-06 21:01:19 +0300

    nonPrimitives added

---
+++ JSFOUNDATION/part2/nonPrimitives,.js
@@ -0,0 +1,24 @@
+const username = {
+  "first name": "Ramazan",
+  isLoggedin: true,
+};
+
+//username.firstname = "Mr. R";
+username.lastname = "Ozturk";
+
+//console.log(username["first name"]);
+//console.log(username.lastname);
+//console.log(username);
+//console.log(typeof username);
+
+let today = new Date();
+//console.log(today.getDate());
+
+// Array
+
+let anotherUser = ["Ramazan", true];
+
+//console.log(anotherUser[0]);
+
+isValue = true;
+console.log(Number(isValue));
