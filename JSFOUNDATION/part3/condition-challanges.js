86cbfceb468a69ade9f91acc94c5d9afdba603a4
Ramazan Öztürk <rozturk3451@gmail.com>
2025-02-12 22:30:19 +0300

    js conditions added

---
+++ JSFOUNDATION/part3/condition-challanges.js
@@ -0,0 +1,61 @@
+// Checking if a number is greater than another number:
+
+let num1 = 5;
+let num2 = 8;
+
+/*
+console.log("I am regular upper code");
+if (num1 > num2) {
+  console.log("num1 is greater than num2");
+} else {
+  console.log("Nope num1 is not greater");
+}
+
+console.log("I am regular bottom code");
+*/
+// Checking if a string is equal to another string:
+
+let username = "Ramazan";
+let anotherUsername = "Ramazan";
+
+/*
+if (username == anotherUsername) {
+  console.log("pick another username");
+} else {
+  console.log("you can pick this username");
+}
+*/
+
+// Checking if a variable is a number or not:
+
+let score = 44;
+
+/*
+if (typeof score === "number") {
+  console.log("Yep, this is a number");
+} else {
+  console.log("No that is a not number");
+}
+*/
+
+// Checking if a boolean value is true or false:
+
+let isTeaReady = false;
+
+if (isTeaReady) {
+  console.log("Tea is Ready");
+} else {
+  //console.log("Tea is not ready");
+}
+
+// Checking if an array is empty or not
+
+let items = ["item1"];
+
+console.log(items.length);
+
+if (items.length === 0) {
+  console.log("Array is empty");
+} else {
+  console.log("Array is not empty");
+}
