0ddba299b8eae31aac25125a2964e28c71b15f18
Ramazan Öztürk <rozturk3451@gmail.com>
2025-04-14 14:49:55 +0300

    new challanges added

---
+++ JSFOUNDATION/part4/levelUpChallanges.js
@@ -0,0 +1,33 @@
+let teas = ["green tea", "black tea", "chai", "oolong tea"];
+let selectedTeas = [];
+
+for (let i = 0; i < teas.length; i++) {
+  if (teas[i] === "chai") {
+    break;
+  }
+  selectedTeas.push(teas[i]);
+}
+
+//console.log(selectedTeas);
+
+let cities = ["London", "New York", "Paris", "Berlin"];
+let visitedCities = [];
+
+for (let i = 0; i < cities.length; i++) {
+  if (cities[i] === "Paris") {
+    continue;
+  }
+  visitedCities.push(cities[i]);
+}
+//console.log(visitedCities);
+
+let numbers = [1, 2, 3, 4, 5];
+let smallNumbers = [];
+
+for (const num of numbers) {
+  if (num === 4) {
+    break;
+  }
+  smallNumbers.push(num);
+}
+//console.log(smallNumbers);
