ca38a566774f420b601fe7a531ccc9896ee22858
Ramazan Öztürk <rozturk3451@gmail.com>
2025-05-03 16:53:44 +0300

    new challanges added

--- JSFOUNDATION/part4/levelUpChallanges.js
+++ JSFOUNDATION/part4/levelUpChallanges.js
@@ -31,3 +31,52 @@ for (const num of numbers) {
   smallNumbers.push(num);
 }
 //console.log(smallNumbers);
+let teaTypes = ["chai", "green tea", "herbal tea", "black tea"];
+let preferredTeas = [];
+
+for (const tea of teaTypes) {
+  if (tea === "herbal tea") {
+    continue;
+  }
+  preferredTeas.push(tea);
+}
+//console.log(preferredTeas);
+
+let citiesPopulation = {
+  London: 8900000,
+  "New York": 8400000,
+  Paris: 2200000,
+  Berlin: 3500000,
+};
+
+let citiesNewPopulations = {};
+
+for (const city in citiesPopulation) {
+  if (city == "Berlin") {
+    break;
+  }
+  citiesNewPopulations[city] = citiesPopulation[city];
+}
+//console.log(citiesNewPopulations);
+
+let worldCities = {
+  Sydney: 5000000,
+  Tokyo: 9000000,
+  Berlin: 3500000,
+  Paris: 2200000,
+};
+let largeCities = {};
+
+for (const city in worldCities) {
+  if (worldCities[city] < 3000000) {
+    continue;
+  }
+  largeCities[city] = worldCities[city];
+}
+//console.log(largeCities);
+
+let teaCollection = ["earl grey", "green tea", "chai", "oolong tea"];
+
+let availableTeas = [];
+
+teaCollection.forEach((tea) => {});
